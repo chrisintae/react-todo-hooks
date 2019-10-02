bfe84706cd7a3c5d3fc40b12600d49a6bafd1705
Chris Intae Fleming <cfleming@athenahealth.com>
2019-10-02 11:51:23 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react' // import useState Hook
+
+// create component
+function Todo({ todo, index }) {
+    return (
+        <div className='todo'>
+            { todo.text }
+        </div>
+    )
+}
+
+
+// useState gives two variables
+// 1. value of state: this.state w/in class
+// 2. update state: this.setState w/in class
+
+function App () {
+    // todos = state
+    // setTodos = method to update state
+    const [ todos, setTodos ] = useState([ //array of objects
+        {
+            text: 'Learn about React',
+            isCompleted: false
+        },
+        {
+            text: 'Meet friend for lunch',
+            isCompleted: false
+        },
+        {
+            text: 'Build really cool todo app',
+            isCompleted: false
+        }
+    ]);
+
+    return (
+        <div className='app'>
+            <div className='todo-list'>
+                { todos.map((todo, index) => (
+                    <Todo key={ index } index={ index} todo={ todo } />
+                ))}
+            </div>  
+        </div>
+    )
+}
+
+export default App;
